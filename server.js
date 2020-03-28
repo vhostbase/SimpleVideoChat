1dbbe64ff14fc3a2e4ed834251bbb4f7a20b0a9c
venufs <venufs@gmail.com>
2020-03-28 13:28:26 +0530

    upd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebSocketServer = require('ws').Server,
   https = require('https'),
   app = express(),
   fs = require('fs');
-
+var clientSet = {};
 const pkey = fs.readFileSync('./ssl/key.pem'),
   pcert = fs.readFileSync('./ssl/cert.pem'),
   options = {key: pkey, cert: pcert, passphrase: '123456789'};
@@ -28,25 +28,39 @@ console.log("The HTTPS server is up and running");
 wss = new WebSocketServer({server: sslSrv});  
 console.log("WebSocket Secure server is up and running.");
 
+
+
+
+
+
 /** successful connection */
-wss.on('connection', function (client) {
-  console.log("A new WebSocket client was connected.");
+wss.on('connection', function (client, incoming_request) {
+	var clientId = client._ultron.id;
+	console.log(client.upgradeReq.url.replace('\?', ''));
+	const urlParams = new URLSearchParams(client.upgradeReq.url.replace('/?', ''));
+	const myParam = urlParams.get('yuid');
+	clientSet[myParam]=client;
+  console.log("A new WebSocket client was connected."+clientId);
   /** incomming message */
   client.on('message', function (message) {
+	  var msgData = JSON.parse(message);
+	  var id = msgData.yuid;
+	  var target = clientSet[id];
     /** broadcast message to all clients */
-    wss.broadcast(message, client);
+    wss.broadcast(message, client, target);
   });
 });
 // broadcasting the message to all WebSocket clients.
-wss.broadcast = function (data, exclude) {
+wss.broadcast = function (data, exclude, target) {
   var i = 0, n = this.clients ? this.clients.length : 0, client = null;
   if (n < 1) return;
   console.log("Broadcasting message to all " + n + " WebSocket clients.");
   for (; i < n; i++) {
-    client = this.clients[i];
-    // don't send the message to the sender...
-    if (client === exclude) continue;
-    if (client.readyState === client.OPEN) client.send(data);
-    else console.error('Error: the client state is ' + client.readyState);
+	client = this.clients[i];
+	// don't send the message to the sender...
+	if (client === exclude) continue;
+	if(target !== client)continue;
+	if (client.readyState === client.OPEN) client.send(data);
+	else console.error('Error: the client state is ' + client.readyState);
   }
 };
