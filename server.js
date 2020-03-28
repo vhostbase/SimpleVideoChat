75e4fbafdf7138080420f86cf0a43ebf5374a730
venufs <venufs@gmail.com>
2020-03-28 15:12:14 +0530

    upd

--- server.js
+++ server.js
@@ -27,41 +27,35 @@ console.log("The HTTPS server is up and running");
 // create the WebSocket server
 wss = new WebSocketServer({server: sslSrv});  
 console.log("WebSocket Secure server is up and running.");
-
-
-
-
-
-
 /** successful connection */
 wss.on('connection', function (client, incoming_request) {
 	var clientId = client._ultron.id;
 	console.log(client.upgradeReq.url.replace('\?', ''));
 	const urlParams = new URLSearchParams(client.upgradeReq.url.replace('/?', ''));
 	const myParam = urlParams.get('yuid');
-	clientSet[myParam]=client;
+	clientSet[myParam]=clientId;
   console.log("A new WebSocket client was connected."+clientId);
   /** incomming message */
   client.on('message', function (message) {
 	  var msgData = JSON.parse(message);
 	  var id = msgData.yuid;
-	  var target = clientSet[id];
+	  var tgtClientId = clientSet[id];
     /** broadcast message to all clients */
-    wss.broadcast(message, client, target);
+    wss.broadcast(message, client, tgtClientId);
   });
 });
 // broadcasting the message to all WebSocket clients.
-wss.broadcast = function (data, exclude, target) {
+wss.broadcast = function (data, exclude, tgtClientId) {
   var i = 0, n = this.clients ? this.clients.length : 0, client = null;
   if (n < 1) return;
   console.log("Broadcasting message to all " + n + " WebSocket clients.");
-  for (; i < n; i++) {
+  for (i=0; i < n; i++) {
 	client = this.clients[i];
 	console.log("client id "+client._ultron.id);
-	console.log("target id "+target._ultron.id);
+	console.log("target id "+tgtClientId);
 	// don't send the message to the sender...
 	if (client._ultron.id === exclude._ultron.id) continue;
-	if(target._ultron.id === client._ultron.id){
+	if(tgtClientId === client._ultron.id){
 		console.log('Found Client...');
 		if (client.readyState === client.OPEN) client.send(data);
 		else console.error('Error: the client state is ' + client.readyState);
